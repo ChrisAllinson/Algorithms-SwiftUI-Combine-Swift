4924da72571c09f9d0bf47692b9446c0d98e599b
Chris Allinson <allinson.ca@hotmail.com>
2023-12-21 20:28:23 -0700

    Initial app project

---
+++ Algorithms-SwiftUI-Combine-Swift/Algorithms-SwiftUI-Combine-Swift/Algorithms_SwiftUI_Combine_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Algorithms_SwiftUI_Combine_SwiftApp.swift
+//  Algorithms-SwiftUI-Combine-Swift
+//
+//  Created by Chris Allinson on 2023-12-21.
+//
+
+import SwiftUI
+
+@main
+struct Algorithms_SwiftUI_Combine_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
